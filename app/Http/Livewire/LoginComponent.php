fbfc9cae606d07da89c1e28bec4bda52066c37fb
Ahmed Ashraf <ahmed29329@gmail.com>
2020-03-14 01:37:09 +0200

    reload login component

--- app/Http/Livewire/LoginComponent.php
+++ app/Http/Livewire/LoginComponent.php
@@ -6,6 +6,10 @@ use Livewire\Component;
 
 class LoginComponent extends Component
 {
+    public function refresh()
+    {
+        // just to reload the ui and check @auth
+    }
     public function render()
     {
         return view('livewire.login-component');
--- resources/views/livewire/login-component.blade.php
+++ resources/views/livewire/login-component.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div wire:init="refresh">
     @auth
     <a class="px-3 py-2 rounded-md text-sm font-medium text-white bg-gray-800 focus:outline-none focus:text-white focus:bg-gray-700" href="#">
         Weclome {{ auth()->user()->name }}
